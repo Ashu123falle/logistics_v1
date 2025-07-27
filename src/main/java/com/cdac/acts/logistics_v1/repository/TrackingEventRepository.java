e5a23a2f8df07881b34486c241e47f5693b0e6ea
Ashu123falle <ashutoshfalle28@gmail.com>
2025-07-27 12:45:28 +0530

    added basic repo implementation

---
+++ src/main/java/com/cdac/acts/logistics_v1/repository/TrackingEventRepository.java
@@ -0,0 +1,9 @@
+package com.cdac.acts.logistics_v1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cdac.acts.logistics_v1.model.TrackingEvent;
+
+public interface TrackingEventRepository extends JpaRepository<TrackingEvent, Long>{
+
+}
