0576b5528ae470090dbdc6dae570932f40b36bad
Ashu123falle <ashutoshfalle28@gmail.com>
2025-07-29 21:24:15 +0530

    added new custom methods

--- src/main/java/com/cdac/acts/logistics_v1/repository/InvoiceRepository.java
+++ src/main/java/com/cdac/acts/logistics_v1/repository/InvoiceRepository.java
@@ -2,8 +2,8 @@ package com.cdac.acts.logistics_v1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cdac.acts.logistics_v1.model.Invoice;
+import com.cdac.acts.logistics_v1.model.Payment;
 
-public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
+public interface InvoiceRepository extends JpaRepository<Payment, Long>{
 
 }
